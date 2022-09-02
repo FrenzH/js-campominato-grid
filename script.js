42348122c576843456a4352a1f9f580e1e53555e
FrenzH <franciattom99@gmail.com>
2022-09-02 15:48:06 +0100

    click-function-btn

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-console.log('JS OK!'
-)
+console.log('JS OK!')
+
+const button = document.getElementById('generator-btn');
+const grid = document.getElementById('grid');
+button.addEventListener('click', CellsGenerator());
+
+
+
+
+
+
+
+
+
+
+
+
+function CellsGenerator (){
+    
+    
+    for(let i=1 ; i <= 100 ; i++){
+        const cells = document.createElement('div');
+        grid.append(cells)
+    }
+}
+
+
+function getRandomNumber(min,max){
+    const range = max - min; 
+    const random = Math.floor(Math.random()*range)  
+    const result = min + random;
+    return result;
+}
